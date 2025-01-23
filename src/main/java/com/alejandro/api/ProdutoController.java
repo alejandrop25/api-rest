6b81466b36d93f51efc7c5c07fc4aabd1b4a6efa
Alejandro Oliveira Perrone <38303096+alejandrop25@users.noreply.github.com>
2025-01-22 21:09:01 -0300

    Add files via upload

---
+++ src/main/java/com/alejandro/api/ProdutoController.java
@@ -0,0 +1,23 @@
+package com.alejandro.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+    @Autowired
+    private ProdutoRepository repository;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Produto adicionar(@RequestBody Produto produto) {
+        return repository.save(produto);
+    }
+}
---
+++ src/main/java/com/alejandro/api/ProdutoRepository.java
@@ -0,0 +1,5 @@
+package com.alejandro.api;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
